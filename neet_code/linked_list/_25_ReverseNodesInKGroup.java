d60612292d757c02a58991a65cdc9750b666e122
trung-nh <trungnh.ict@gmail.com>
2025-07-25 00:16:58 +0700

    neetcode/linked-list: reverse nodes in k-group, Jul 25 2025

---
+++ neet_code/linked_list/_25_ReverseNodesInKGroup.java
@@ -0,0 +1,29 @@
+package neet_code.linked_list;
+
+public class _25_ReverseNodesInKGroup {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        //1. test weather we have more then k node left, if less then k node left we just return head
+        ListNode node = head;
+        int count = 0;
+        while (count < k) {
+            if (node == null)
+                return head;
+            node = node.next;
+            count++;
+        }
+        // 2.reverse k node at current level
+        ListNode pre = reverseKGroup(node, k); //pre node point to the the answer of sub-problem
+        while (count > 0) {
+            ListNode next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+            count = count - 1;
+        }
+        return pre;
+    }
+}
